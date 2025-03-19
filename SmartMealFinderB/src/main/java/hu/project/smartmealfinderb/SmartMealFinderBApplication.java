1b2a3934234a901d774f45e9f127065e20587503
XZD47P <kutizoli2003@gmail.com>
2025-03-19 16:15:37 +0100

    Created backend Java project

---
+++ SmartMealFinderB/src/main/java/hu/project/smartmealfinderb/SmartMealFinderBApplication.java
@@ -0,0 +1,13 @@
+package hu.project.smartmealfinderb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMealFinderBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMealFinderBApplication.class, args);
+    }
+
+}
---
+++ SmartMealFinderB/src/test/java/hu/project/smartmealfinderb/SmartMealFinderBApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.project.smartmealfinderb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMealFinderBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
