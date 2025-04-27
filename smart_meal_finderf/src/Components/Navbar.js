07c5dfdcb02946c0b28a364e02a94653a4c76647
XZD47P <kutizoli2003@gmail.com>
2025-04-27 11:42:08 +0200

    Added My Plan button, if the user is logged in

--- smart_meal_finderf/src/Components/Navbar.js
+++ smart_meal_finderf/src/Components/Navbar.js
@@ -28,10 +28,25 @@ const Navbar = () => {
         <header
             className={"h-headerHeight z-50 text-textColor bg-headerColor shadow-sm flex items-center sticky top-0"}>
             <nav className={"sm:px-10 px-4 flex w-full h-full items-center justify-between"}>
-                <Link to="/">
-                    {" "}
-                    <h3 className={"font-dancingScript text-logoText"}>Smart Meal Finder</h3>
-                </Link>
+                <div className="flex items-center gap-4">
+                    <Link to="/">
+                        {" "}
+                        <h3 className={"font-dancingScript text-logoText"}>Smart Meal Finder</h3>
+                    </Link>
+
+                    {token ? (
+                        <>
+                            <Link className={`py-2 cursor-pointer hover:text-white ${
+                                pathName === "/plan" ? "font-semibold " : ""}`}
+                                  to="/plan"
+                            >
+                                My Plan
+                            </Link>
+                        </>
+                    ) : (
+                        <></>
+                    )}
+                </div>
                 <ul
                     className={`lg:static  absolute left-0  top-16 w-full lg:w-fit lg:px-0 sm:px-10 px-4  lg:bg-transparent bg-headerColor   
                     ${
