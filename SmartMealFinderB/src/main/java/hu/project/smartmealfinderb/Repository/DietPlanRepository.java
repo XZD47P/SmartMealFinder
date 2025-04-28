364b80b6da54b899a13f853906ee0c9cb358bd3d
XZD47P <kutizoli2003@gmail.com>
2025-04-28 11:02:19 +0200

    Added @Repository to DietPlanRepository.java and VerificationTokenRepository.java

--- SmartMealFinderB/src/main/java/hu/project/smartmealfinderb/Repository/DietPlanRepository.java
+++ SmartMealFinderB/src/main/java/hu/project/smartmealfinderb/Repository/DietPlanRepository.java
@@ -3,9 +3,11 @@ package hu.project.smartmealfinderb.Repository;
 import hu.project.smartmealfinderb.Model.DietPlan;
 import hu.project.smartmealfinderb.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DietPlanRepository extends JpaRepository<DietPlan, Long> {
 
     Optional<DietPlan> findByUserId(User userId);
