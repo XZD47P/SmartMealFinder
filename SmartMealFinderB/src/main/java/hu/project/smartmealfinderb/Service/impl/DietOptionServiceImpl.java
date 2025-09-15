47f85f14cd91ea9913bdcdefa72c0759032979b5
XZD47P <kutizoli2003@gmail.com>
2025-09-15 22:41:04 +0200

    Added @Transactional to DietOptionServiceImpl.addDietOptionToUser

--- SmartMealFinderB/src/main/java/hu/project/smartmealfinderb/Service/impl/DietOptionServiceImpl.java
+++ SmartMealFinderB/src/main/java/hu/project/smartmealfinderb/Service/impl/DietOptionServiceImpl.java
@@ -8,6 +8,7 @@ import hu.project.smartmealfinderb.Repository.UserDietOptionRepository;
 import hu.project.smartmealfinderb.Service.DietOptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,6 +49,7 @@ public class DietOptionServiceImpl implements DietOptionService {
     }
 
     @Override
+    @Transactional
     public void addDietOptionToUser(User user, List<String> diets) {
         List<DietOption> dietOptions = new ArrayList<>();
 
