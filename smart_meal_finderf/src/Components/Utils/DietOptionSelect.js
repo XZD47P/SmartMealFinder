41d4c07f2ad60fee1197916a699451966690b32f
XZD47P <kutizoli2003@gmail.com>
2025-09-18 17:12:05 +0200

    Added choosing the the saved diets as a default, if the user has saved diets

--- smart_meal_finderf/src/Components/Utils/DietOptionSelect.js
+++ smart_meal_finderf/src/Components/Utils/DietOptionSelect.js
@@ -2,25 +2,42 @@ import {useEffect, useState} from "react";
 import Select from "react-select";
 import api from "../../Backend/api";
 import toast from "react-hot-toast";
+import {useMyContext} from "../../Store/ContextApi";
 
 const DietOptionSelect = ({value, onChange, placeholder}) => {
     const [dietOptions, setDietOptions] = useState([]);
+    const {currentUser} = useMyContext();
 
     useEffect(() => {
-        const fetchDietOptions = async () => {
+        const init = async () => {
             try {
+                //Az összes opció lekérése
                 const response = await api.get("/diet-option/list");
                 const mappedDietOptions = response.data.map(item => ({
                     label: item.label,
                     value: item.apiValue,
                 }));
                 setDietOptions(mappedDietOptions);
+
+                // A felhasználó mentett diétáinak betöltése, ha van
+                if (currentUser) {
+                    const dietResponse = await api.get("/diet-option/load-by-user");
+                    const userDiets = dietResponse.data;
+
+                    const defaultDietOptions = mappedDietOptions.filter(option =>
+                        userDiets.includes(option.value)
+                    );
+
+                    onChange(defaultDietOptions);
+                }
             } catch (error) {
                 toast.error("There was an error while retrieving diet options");
             }
         };
-        fetchDietOptions();
-    }, []);
+
+        init();
+    }, [currentUser, onChange]);
+
 
     return (
         <div className="mb-4">
