d2f88283e62fb8e15d1eaec2b1023b51b1814d1f
XZD47P <kutizoli2003@gmail.com>
2025-11-26 19:44:34 +0100

    Removed unnecessary @Async annotations

--- SmartMealFinderB/src/main/java/hu/project/smartmealfinderb/Service/ProfilingService.java
+++ SmartMealFinderB/src/main/java/hu/project/smartmealfinderb/Service/ProfilingService.java
@@ -2,15 +2,12 @@ package hu.project.smartmealfinderb.Service;
 
 import hu.project.smartmealfinderb.DTO.Response.SpoonacularRecipeResp;
 import hu.project.smartmealfinderb.Model.User;
-import org.springframework.scheduling.annotation.Async;
 
 public interface ProfilingService {
-    @Async
+
     void sendItemToGorse(SpoonacularRecipeResp recipe);
 
-    @Async
     void sendUserToGorse(User user);
-
-    @Async
+    
     void deleteUserFromGorse(User user);
 }
